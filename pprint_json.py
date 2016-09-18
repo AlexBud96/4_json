af62014bbb232154c58fa148d48a1f1f125f0f47
AlexBud96 <AlexSy4ov@gmail.com>
2016-09-18 19:56:23 +0000

    Путь до файла вводится вторым аргументом командной строки

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,18 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    global data
+    with open(filepath, 'r') as f:
+        data = json.load(f)
 
 
 def pretty_print_json(data):
-    pass
+    parsed = data
+    print (json.dumps(parsed, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    load_data(sys.argv[1])
+    pretty_print_json(data)
